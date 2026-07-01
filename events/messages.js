78a3f500364b2ff2794efab8e7130d1bf35e518b
original-void <void321m@gmail.com>
2026-07-01 11:42:54 +0300

    Update messages.js

--- events/messages.js
+++ events/messages.js
@@ -1,20 +1,12 @@
-const commandHandler = require("../lib/commandHandler");
-
 module.exports = (sock) => {
 
     sock.ev.on("messages.upsert", async ({ messages }) => {
 
+        console.log("EVENT FIRED");
+
         const msg = messages[0];
 
-        console.log("New message received");
-
-        if (!msg.message) return;
-
-        if (msg.key.fromMe) return;
-
         console.log(JSON.stringify(msg, null, 2));
-
-        await commandHandler(sock, msg);
 
     });
 
