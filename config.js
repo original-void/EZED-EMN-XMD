14d59064ced0967b05d8541ce06d0e6bfa5e8e82
original-void <void321m@gmail.com>
2026-07-01 10:06:19 +0300

    Add configuration file for bot settings

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    PREFIX: ".",
+
+    BOT_NAME: "EZED EMN XMD",
+
+    OWNER_NAME: "EZED NYANUGA",
+
+    OWNER_NUMBER: "254769532338",
+
+    FOOTER: "Powered by EZED NYANUGA",
+
+    VERSION: "1.0.0"
+};
