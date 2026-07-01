9c7146071364311ee7c7d95367611852a9373872
original-void <void321m@gmail.com>
2026-07-01 11:31:10 +0300

    Refactor message upsert handler to simplify logic
    Removed type check for message notifications and added logging for new messages.

--- events/messages.js
+++ events/messages.js
@@ -2,15 +2,17 @@ const commandHandler = require("../lib/commandHandler");
 
 module.exports = (sock) => {
 
-    sock.ev.on("messages.upsert", async ({ messages, type }) => {
-
-        if (type !== "notify") return;
+    sock.ev.on("messages.upsert", async ({ messages }) => {
 
         const msg = messages[0];
+
+        console.log("New message received");
 
         if (!msg.message) return;
 
         if (msg.key.fromMe) return;
+
+        console.log(JSON.stringify(msg, null, 2));
 
         await commandHandler(sock, msg);
 
